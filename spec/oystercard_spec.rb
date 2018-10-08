04205994fd672450a240ebbf57f2522b32ccac45
sacullezzar <lucas.razzell@holidayextras.com>
2018-10-08 15:23:40 +0100

    Challenge #5: top up function

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,14 @@ describe Oystercard do
   it "should have a balance of 0" do
     expect(subject.balance).to eq(0)
   end
+
+  it "can be topped up" do
+    expect(subject).to respond_to(:top_up).with(1).argument
+  end
+
+  it "increases the balance of a card" do
+    card = Oystercard.new
+    card.top_up(20)
+    expect(card.balance).to eq 20
+  end
 end
