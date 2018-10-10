c3c446a9bf0e0eb0051b20a0cb79c2e78f2e4a26
sacullezzar <lucas.razzell@holidayextras.com>
2018-10-10 09:46:53 +0100

    End of Step 11

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,28 +1,54 @@
 class Oystercard
 
   MAX_BALANCE = 90
+  MIN_BALANCE = 1
+  MIN_CHARGE = 1
 
-  attr_reader :balance
-
+  attr_reader :balance, :status, :starting_station
+  
   def initialize
     @balance = 0
+    @status = false
+    @starting_station = nil
   end
 
-  def top_up(val)
-    fail "Maximum balance of #{MAX_BALANCE} exceeded!" if balance + val > MAX_BALANCE
-    @balance += val
+  def top_up(value)
+    fail "Max balance of #{ MAX_BALANCE }." unless(@balance + value) <= MAX_BALANCE
+    @balance += value
   end
 
-  def deduct(fare)
+  def touch_in(name)
+    fail "Balance below #{MIN_BALANCE}" if self.check_balance < MIN_BALANCE
+    @status = true
+    @starting_station = name
+  end
+
+  def touch_out
+    pay(MIN_CHARGE)
+    @status = false
+    @starting_station = nil
+  end
+
+  def journey?
+    true unless(@starting_station ? nil : false)
+  end
+
+  def check_balance
+    @balance
+  end
+
+  private
+
+  def pay(fare)
     @balance -= fare
   end
 
-  def in_journey?
-    @status
-  end
-
-  def touch_in
-    @status = true
-  end
-
 end
+
+# class Station
+
+#   def initialize(name)
+#     @name = name
+#   end
+
+# end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,40 +1,60 @@
-require "./lib/oystercard.rb"
+require './lib/oystercard.rb'
 
 describe Oystercard do
 
-  it "should have a balance of 0" do
-    expect(subject.balance).to eq(0)
+  it 'should have an initialisation balance of 0' do
+    expect(subject.balance).to eq 0
   end
-  describe "#top_up" do
-    it { is_expected.to respond_to(:top_up).with(1).argument }
+end
 
-    # it "increases the balance of a card" do
-    #   subject.deduct(30)
-    # expect{ subject.top_up 20 }.to change{ subject.balance }.by 20
-    # end
 
-    before(:each) { subject.top_up(Oystercard::MAX_BALANCE) }
+describe Oystercard do
+  let(:station) { double :station }
+  #before(:each) {skip("Awaiting code refactor.") }
+  before(:each) { subject.top_up(10) }
 
-    it "raises an error when maximum balance is exceeded" do
-      maximum_balance = Oystercard::MAX_BALANCE
-      expect{ subject.top_up 1 }.to raise_error "Maximum balance of #{maximum_balance} exceeded!"
-    end
+  it 'should allow us to top_up the card balance' do
+    expect{ subject.top_up(10) }.to change{ subject.balance }.by 10
   end
 
-  describe "#deduct" do
-    it { is_expected.to respond_to(:deduct).with(1).argument }
-
-    it "deducts an amount from the balance" do
-      expect{ subject.deduct 20 }.to change{ subject.balance }.by -20
-    end
+  it 'should not allow us to have a balance of greater than 90' do
+    maximum_balance = Oystercard::MAX_BALANCE
+    expect{ subject.top_up(100) }.to raise_error "Max balance of #{ maximum_balance }."
   end
 
-  it "is initially not in a journey" do
-    expect(subject).not_to be_in_journey
+  it 'should change status when touched in at start of journey' do
+    expect{ subject.touch_in(station) }.to change{ subject.status }.to eq true
   end
 
-  it "can touch in" do
-    subject.touch_in
-    expect(subject).to be_in_journey
+  it 'should change status when touched out at end of journey' do
+    subject.touch_in(station)
+    expect{ subject.touch_out }.to change{ subject.status }.to eq false
   end
+
+  it 'should report being in a journey or not' do
+  expect(subject.journey?).to eq(true).or eq(false)
+  end
+
+  it "should raise an error if touching in with balance less than the minimum amount " do
+    allow(subject).to receive(:check_balance) { 0 }
+    minimum_balance = Oystercard::MIN_BALANCE
+    expect{subject.touch_in(station)}.to raise_error "Balance below #{minimum_balance}"
+  end
+
+  it "should deduct a fare at the end of a journey" do
+    minimum_charge = Oystercard::MIN_CHARGE
+    expect{ subject.touch_out }.to change{ subject.balance }.by(- minimum_charge)
+  end
+
+  it 'should report starting station after touch_in' do
+    subject.touch_in(station)
+    expect(subject.starting_station).to eq station
+  end
+
+  it 'should forget starting station on touch out, setting it to nil.' do
+    subject.touch_in(station)
+    subject.touch_out
+    expect(subject.starting_station).to eq nil
+  end
+
 end
