e9b1badb8704adc338487b85b9921866490f6e14
sacullezzar <lucas.razzell@holidayextras.com>
2018-10-10 17:28:38 +0100

    mid edge casing

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -24,25 +24,30 @@ class Oystercard
 
   def touch_in(name)
     fail "Balance below #{MIN_BALANCE}" if self.check_balance < MIN_BALANCE
+    puts "Penalty #{journey?}" if (journey?)
     @status = true
+    @exit_station = nil
     @starting_station = name
     @station_zone = lookup_zone(name)
-    @journey = Hash.new
-    @journey["Entry"] = name
-    @exit_station = nil
+    @journey["Entry"] = @starting_station
+    @journey_list.push @journey
   end
 
   def touch_out(name)
     pay(MIN_CHARGE)
+    puts "Penalty" if(journey?)
     @status = false
     @starting_station = nil
     @exit_station = name
-    @journey["Exit"] = name
+    @journey["Exit"] = @exit_station
     @journey_list.push @journey
+  #  @journey = Hash.new
   end
 
   def journey?
-    true unless(@starting_station ? nil : false)
+    puts @starting_station
+    #true unless(@starting_station ? nil : false)
+    @starting_station == nil ? false : true
   end
 
   def check_balance
